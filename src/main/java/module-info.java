5e049043ff75638dcd0fdf4dad4a6d23ec6df24b
Oana Spatacean <oana.spatacean@student.upt.ro>
2023-04-21 23:48:18 +0300

    updated the module-info.java file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,10 @@ module com.example.onlinequizapplication {
 
     opens com.example.onlinequizapplication to javafx.fxml;
     exports com.example.onlinequizapplication;
+    //exports controllers;
+    //opens controllers to javafx.fxml;
+    exports com.example.onlinequizapplication.controllers;
+    opens com.example.onlinequizapplication.controllers to javafx.fxml;
+    exports com.example.onlinequizapplication.exceptions;
+    opens com.example.onlinequizapplication.exceptions to javafx.fxml;
 }
