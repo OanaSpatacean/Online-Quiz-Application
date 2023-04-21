323ea6f83d735356b9e9cd9859ca0f307e978353
Oana Spatacean <oana.spatacean@student.upt.ro>
2023-04-21 23:46:53 +0300

    made RegistrationController

---
+++ src/main/java/com/example/onlinequizapplication/controllers/RegistrationController.java
@@ -0,0 +1,36 @@
+package com.example.onlinequizapplication.controllers;
+
+import com.example.onlinequizapplication.exceptions.UsernameAlreadyExistsException;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import com.example.onlinequizapplication.services.*;
+
+public class RegistrationController {
+
+    @FXML
+    private Text registrationMessage;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private ChoiceBox<String> role;
+
+    @FXML
+    public void initialize() {
+        role.getItems().addAll("Client", "Admin");
+    }
+
+    @FXML
+    public void handleRegisterAction() {
+        try {
+            UserService.addUser(usernameField.getText(), passwordField.getText(), role.getValue());
+            registrationMessage.setText("Account created successfully!");
+        } catch (UsernameAlreadyExistsException e) {
+            registrationMessage.setText(e.getMessage());
+        }
+    }
+}
