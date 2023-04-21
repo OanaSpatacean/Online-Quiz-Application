51d482e5707249287fdca1e0a1a7bfca7b7b8bca
Oana Spatacean <oana.spatacean@student.upt.ro>
2023-04-21 23:16:43 +0300

    imported org.dizitart.no2.Nitrite

--- src/main/java/com/example/onlinequizapplication/HelloApplication.java
+++ src/main/java/com/example/onlinequizapplication/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.dizitart.no2.Nitrite;
 
 import java.io.IOException;
 
@@ -15,9 +16,18 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
+        // Create a Nitrite database instance
+        Nitrite db = Nitrite.builder()
+                .filePath("/Users/Yoga/OneDrive/Desktop/Online-Quiz-Application/myNitriteDatabase.db")
+                .openOrCreate();
+
         launch();
+
+        // Close the Nitrite database instance when the application exits
+        db.close();
     }
 }
